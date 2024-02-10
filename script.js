4e89a8ede08e4bd68e93d576aa5372bca3d5ac60
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-02-10 10:29:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+
+document.getElementById("btn").onclick=fibonacci;
+
 function fibonacci(num) {
-// your code here
+    num = document.getElementById("fibo").value;
+    console.log(num);
+    
+    let a = 0,b=1,c;
+    if(num==1){
+        c=0;
+    }
+    else if(num==2 || num==3){c=1;}
+    else {
+        for (let i = 2; i < num; i++) {
+            c = a + b;
+            console.log(c);
+            a = b;
+            b = c;
+        }
+    }
+    document.getElementById("Res").innerHTML=c;
 }
 
 module.exports = fibonacci;
